11b453a941de6e052e6b59fb7143f6f5239856e4
masterUNG@abc.com <student02@Student02.local>
2019-03-18 14:28:12 +0700

    Start Data Type

---
+++ DtatTypeOfVariable.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+//String Type ตัวแปรที่เป็นอักษร
+
+var myName = "Doramon"  //นี่คือการประกาศ ที่อาศัยชนิดชอง  Value บอก Datatype
+var mySumame: String = "AAAAAA"  // นี่คือการประกาศแบบ กำหนด Datatype  ตั้งแต่เกิด
+
+
+
+
+
---
+++ VarialbAndContant.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+//Explicit Variable
+//การประการศตัวแปร นะจ้ะ
+
+var myName = "Chiraphon"
+myName = "Doramon"
+
+// Expilcit Constant
+let myFriend = "Nopita"
+//myFriend = "Sunako"Error BecausevmyFriend เพราะ myFriend เป็น คราคงที่
+
